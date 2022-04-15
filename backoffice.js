c6238cdca8419f3c13e804e30d23cc991c30616a
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-04-16 00:09:57 +0300

    post and put method added

---
+++ backoffice.js
@@ -0,0 +1,125 @@
+`use strict`;
+const productId = new URLSearchParams(window.location.search).get("productId");
+const endpoint = productId
+  ? "https://striveschool-api.herokuapp.com/api/product/" + productId
+  : "https://striveschool-api.herokuapp.com/api/product/";
+const method = productId ? "PUT" : "POST";
+window.onload = () => {
+  handleEdit();
+};
+const handleSubmit = async (event) => {
+  event.preventDefault();
+
+  const myDetail = {
+    name: document.getElementById("name").value,
+    brand: document.getElementById("brand").value,
+    imageUrl: document.getElementById("image").value,
+    description: document.getElementById("product-description").value,
+    price: document.getElementById("price").value,
+  };
+  try {
+    const response = await fetch(endpoint, {
+      method,
+      body: JSON.stringify(myDetail),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDgxN2M1ZWU3ODE4NzAwMTVjMjY3YTgiLCJpYXQiOjE2NDk5Njc2NjEsImV4cCI6MTY1MTE3NzI2MX0.bJ-a8RydRHuvCgXmMewdmveQbMkVWZnCshZjuA2SYwQ",
+      },
+    });
+    if (response.ok) {
+      const products = await response.json();
+      if (productId) {
+        showAlert(
+          "info",
+          `Item with the ID of ${products._id} was successfully EDITED`
+        );
+        setTimeout(() => {
+          window.location.assign("/");
+        }, 3000);
+      } else {
+        showAlert("success", `New item added with the ID of ${products._id}`);
+        setTimeout(() => {
+          window.location.assign("/");
+        }, 3000);
+      }
+    }
+  } catch (error) {
+    showAlert("danger", err.message);
+  }
+};
+const handleEdit = async () => {
+  try {
+    if (productId) {
+      const response = await fetch(endpoint, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDgxN2M1ZWU3ODE4NzAwMTVjMjY3YTgiLCJpYXQiOjE2NDk5Njc2NjEsImV4cCI6MTY1MTE3NzI2MX0.bJ-a8RydRHuvCgXmMewdmveQbMkVWZnCshZjuA2SYwQ",
+        },
+      });
+      const { name, brand, imageUrl, price, description } =
+        await response.json();
+      document.getElementById("name").value = name;
+      document.getElementById("brand").value = brand;
+      document.getElementById("image").value = imageUrl;
+      document.getElementById("product-description").value = description;
+      document.getElementById("price").value = price;
+      const submitBtn = document.querySelector("button[type='submit']");
+      const delBtn = document.querySelector(".del-btn");
+      const h1 = document.querySelector("h1");
+      h1.innerText = " Edit Item";
+      submitBtn.className = "btn btn-info";
+      submitBtn.innerText = "Edit";
+      delBtn.classList.remove("d-none");
+    }
+  } catch (err) {
+    showAlert("danger", err.message);
+  }
+};
+const validate = (event) => {
+  event.target.form.classList.add("validated");
+};
+
+const showAlert = (type, msg) => {
+  const alertContainer = document.querySelector(".alert-box");
+  alertContainer.innerHTML = `
+  <div class="alert alert-${type}" role="alert">
+  ${msg}
+  </div>
+  `;
+  setTimeout(() => {
+    alertContainer.innerHTML = "";
+  }, 3000);
+};
+
+const handleDelete = async () => {
+  const accepted = confirm("do you want to delete?");
+
+  console.log("Confirmed? ", accepted);
+
+  if (accepted) {
+    try {
+      let response = await fetch(endpoint, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDgxN2M1ZWU3ODE4NzAwMTVjMjY3YTgiLCJpYXQiOjE2NDk5Njc2NjEsImV4cCI6MTY1MTE3NzI2MX0.bJ-a8RydRHuvCgXmMewdmveQbMkVWZnCshZjuA2SYwQ",
+        },
+      });
+
+      if (response.ok) {
+        let deletedObj = await response.json();
+        showAlert(
+          "danger",
+          "Product: " + deletedObj.name + "  successfully deleted"
+        );
+        setTimeout(() => window.location.assign("/"), 3000);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
--- index.js
+++ index.js
@@ -6,28 +6,26 @@ window.onload = () => {
 const option = {
   headers: {
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDgxN2M1ZWU3ODE4NzAwMTVjMjY3YTgiLCJpYXQiOjE2NDk4NTMxNjgsImV4cCI6MTY1MTA2Mjc2OH0.1WzzjhdVLCI-oAC04FAgoyI6jktTW5H14e1Y3LTXc2Y",
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDgxN2M1ZWU3ODE4NzAwMTVjMjY3YTgiLCJpYXQiOjE2NDk5Njc2NjEsImV4cCI6MTY1MTE3NzI2MX0.bJ-a8RydRHuvCgXmMewdmveQbMkVWZnCshZjuA2SYwQ",
   },
 };
 const displayProducts = async () => {
-  const response = await fetch(
-    "https://striveschool-api.herokuapp.com/api/product/",
-    option
-  );
+  const url = "https://striveschool-api.herokuapp.com/api/product/";
+  const response = await fetch(url, option);
   const products = await response.json();
-  console.log(products);
   const row = document.querySelector(`.row`);
   row.innerHTML = ``;
   products.forEach((product) => {
     const col = document.createElement("div");
-    col.classList.add("col-4", "mb-2");
+    col.classList.add("col-sm-6", "col-md-3", "mb-2");
     col.innerHTML = `<div class="card">
     <img src="${product.imageUrl}" class="card-img-top" alt="...">
     <div class="card-body">
     <h5 class="card-title">${product.name}</h5>
     <p class="card-text">${product.brand}</p>
     <p class="card-text">${product.description}</p>
-    <a href="#" class="btn btn-primary">${product.price}</a>
+    <a href="#" class="fw-bold fs-4 text-decoration-none">${product.price}€</a>
+    <a href="./backoffice.html?productId=${product._id}" class="btn btn-primary float-end">Edit</a>
     </div>
     </div>`;
     row.appendChild(col);
